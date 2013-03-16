da931ae3c37f3851de541a6253883e40c8198da5
mcneves <mcn@isep.ipp.pt>
2013-03-16 16:11:50 +0000

    criar arquitetura e teste da eapli.util

---
+++ eapli.ExpenseManager/src/Presentation/ExpenseRegisterUI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+/**
+ *
+ * @author mcn
+ */
+public class ExpenseRegisterUI {
+      
+      public ExpenseRegisterUI(){
+            
+      }
+      public void run(){
+            
+      }
+}
